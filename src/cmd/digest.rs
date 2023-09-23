c2175975825b7d47a3768c776ba32394b41a0640
Wolf McNally <wolf@wolfmcnally.com>
2023-09-23 01:57:00 -0700

    Working.

--- src/cmd/digest.rs
+++ src/cmd/digest.rs
@@ -1,11 +1,30 @@
-use clap::Args;
+use std::collections::HashSet;
+
+use clap::{Args, ValueEnum};
 
 use crate::envelope_args::{EnvelopeArgs, EnvelopeArgsLike};
+use bc_envelope::prelude::*;
+
+#[derive(ValueEnum, Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Depth {
+    /// Return just the envelope's top digest.
+    Top,
+    /// Return the digests necessary to reveal the subject.
+    Shallow,
+    /// Return the digests needed to reveal the entire contents of the envelope.
+    Deep,
+}
 
 /// Print the envelope's digest.
 #[derive(Debug, Args)]
 #[group(skip)]
 pub struct CommandArgs {
+    #[arg(long = "depth", default_value = "top")]
+    depth: Depth,
+
+    #[arg(long = "hex", default_value = "false")]
+    hex: bool,
+
     #[command(flatten)]
     envelope_args: EnvelopeArgs,
 }
@@ -17,7 +36,36 @@ impl EnvelopeArgsLike for CommandArgs {
 }
 
 impl crate::exec::Exec for CommandArgs {
+    // ```swift
+    // mutating func run() throws {
+    //     resetOutput()
+    //     try fill()
+    //     guard let envelope else {
+    //         throw EnvelopeToolError.missingArgument("envelope")
+    //     }
+    //     let digests: Set<Digest>
+    //     switch depth {
+    //     case .top:
+    //         digests = [envelope.digest]
+    //     case .shallow:
+    //         digests = envelope.shallowDigests
+    //     case .deep:
+    //         digests = envelope.deepDigests
+    //     }
+    //     printOut(digests.sorted().map { isHex ? $0.hex : $0.ur.string }.joined(separator: " "))
+    // }
+    // ```
+
     fn exec(&self) -> anyhow::Result<String> {
-        todo!();
+        let envelope = self.get_envelope()?;
+        let digests: HashSet<Digest> = match self.depth {
+            Depth::Top => vec![envelope.digest().into_owned()].into_iter().collect::<HashSet<_>>(),
+            Depth::Shallow => envelope.shallow_digests(),
+            Depth::Deep => envelope.deep_digests(),
+        };
+        let mut ordered_digests = digests.iter().cloned().collect::<Vec<_>>();
+        ordered_digests.sort();
+        let output = ordered_digests.iter().map(|d| if self.hex { d.hex() } else { d.ur_string() }).collect::<Vec<String>>().join(" ");
+        Ok(output)
     }
 }
--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -821,3 +821,26 @@ fn test_assertion_object_find_1() -> anyhow::Result<()> {
         CREDENTIAL_EXAMPLE,
     )
 }
+
+// ```swift
+// func testEnvelopeDigest() throws {
+//     let d = try envelope("digest \(aliceKnowsBobExample)")
+//     XCTAssertEqual(d, "ur:digest/hdcxldgouyhyadimzmpaeourhfsectvaskspdlotaxidiatbgydejnbwgskbhfrtwlwzneroatds")
+// }
+// ```
+
+#[test]
+fn test_envelope_digest() -> anyhow::Result<()> {
+    run_cli_expect(
+        &["digest", ALICE_KNOWS_BOB_EXAMPLE],
+        "ur:digest/hdcxldgouyhyadimzmpaeourhfsectvaskspdlotaxidiatbgydejnbwgskbhfrtwlwzneroatds"
+    )
+}
+
+#[test]
+fn test_envelope_digest_hex() -> anyhow::Result<()> {
+    run_cli_expect(
+        &["digest", "--hex", ALICE_KNOWS_BOB_EXAMPLE],
+        "8955db5e016affb133df56c11fe6c5c82fa3036263d651286d134c7e56c0e9f2"
+    )
+}
